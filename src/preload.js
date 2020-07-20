ec450911e11207b1298c251c2a8ab1869fa1212b
pranavwaykos12 <waykos.pranav@gmail.com>
2020-07-20 10:56:28 +0530

    Removed unused menu and changes menu ui

---
+++ src/preload.js
@@ -0,0 +1,22 @@
+const { remote } = require("electron");
+const {
+  getCurrentWindow,
+  openMenu,
+  minimizeWindow,
+  maximizeWindow,
+  unmaximizeWindow,
+  maxUnmaxWindow,
+  isWindowMaximized,
+  closeWindow
+} = require("./menu-functions");
+
+window.addEventListener("DOMContentLoaded", () => {
+  window.getCurrentWindow = getCurrentWindow;
+  window.openMenu = openMenu;
+  window.minimizeWindow = minimizeWindow;
+  window.maximizeWindow = maximizeWindow;
+  window.unmaximizeWindow = unmaximizeWindow;
+  window.maxUnmaxWindow = maxUnmaxWindow;
+  window.isWindowMaximized = isWindowMaximized;
+  window.closeWindow = closeWindow;
+});
